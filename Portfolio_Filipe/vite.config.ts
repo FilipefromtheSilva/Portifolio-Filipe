d96718b664367c6128191d1985eea5a587b3e387
Filipe da Silva Oliveira <134624107+FilipefromtheSilva@users.noreply.github.com>
2026-03-17 14:29:35 -0300

    FEAT: linha base

--- Portfolio_Filipe/vite.config.ts
+++ Portfolio_Filipe/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Portifolio-Filipe/',  // LINHA CRÍTICA - com barras no início e fim
   server: {
     host: "::",
     port: 8080,
